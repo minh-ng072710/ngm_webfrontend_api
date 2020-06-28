a91a541596b73114bf2f776f35e3e8ce370434c7
minh nguyen <minh.nguyengia@thecoffeehouse.vn>
2020-06-29 00:35:41 +0700

    add more API

--- src/controllers/Admin.Controller.js
+++ src/controllers/Admin.Controller.js
@@ -2,6 +2,7 @@ var admin = require('firebase-admin');
 let serviceAccount = require('../config/key_firebase.json');
 //multer
 var multer = require('multer');
+const { json } = require('body-parser');
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'public/upload')
@@ -15,7 +16,7 @@ var upload = multer({
     storage: storage,
     fileFilter: function (req, file, cb) {
         console.log(file);
-        if (file.mimetype == "image/bmp" || file.mimetype == "image/png" || file.mimetype == "image/jpg") {
+        if (file.mimetype == "image/bmp" || file.mimetype == "image/png" || file.mimetype == "image/jpeg") {
             cb(null, true)
         } else {
             return cb(new Error('Only image are allowed!'))
@@ -39,6 +40,7 @@ var Create_Book = (req, res) => {
     console.log(Date.now())
     let ref = db.collection('Book').doc();
     let id = ref.id;
+
     db.collection('Book').doc(id).set({
         Book_ID: id,
         User_Create: req.body.User_Create,
@@ -48,6 +50,7 @@ var Create_Book = (req, res) => {
         Description: req.body.Description,
         Book_Name: req.body.Book_Name,
         Status: req.body.Status,
+        Active: req.body.Active,
         Time_Created: Date.now(),
         Time_Updated: ''
     });
@@ -55,23 +58,53 @@ var Create_Book = (req, res) => {
 }
 var Update_Book = (req, res) => {
 
-    console.log(req.body)
+    console.log("body nè: " + req.body.Category_Name)
+    console.log("Status: " + req.body.Status)
 
-    db.collection("Book").doc(req.body.Book_ID).update({
-        User_Create: req.body.User_Create,
-        Category: req.body.Category,
-        Publication_Date: req.body.Publication_Date,
-        Description: req.body.Description,
-        Book_Name: req.body.Book_Name,
-        Status: req.body.Status,
-        Time_Updated: ''
+    if (req.body.Category_Name) {
+        db.collection("Book").where('Category', '==', req.body.Category_Name).get().then(function (querySnapshot) {
+            querySnapshot.forEach(doc => {
+                console.log(doc.data())
+                doc.ref.update({
+                    Active: req.body.Status
+                })
+            })
+        }).catch(err => {
+            console.log("Có lỗi: " + err)
+        })
+        // db.collection("Book").doc("ntDNxUc8KsCBMsnm1aXl").update({
+        //     Active: req.body.Status
+        // })
 
-    });
+    } else {
+        db.collection("Book").doc(req.body.Book_ID).update({
+            User_Create: req.body.User_Create,
+            Category: req.body.Category,
+            Publication_Date: req.body.Publication_Date,
+            Description: req.body.Description,
+            Book_Name: req.body.Book_Name,
+            Status: req.body.Status,
+            Time_Updated: '',
+        });
+    }
+
 
     res.send({ status: 200 })
 }
-
-var Get_All = (req, res) => {
+var Get_All_User = (req, res) => {
+    let list = []
+    db.collection('User').get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                list.push(doc.data())
+            });
+            res.send(list)
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err);
+        });
+}
+var Get_All_Book = (req, res) => {
     let list = []
     db.collection('Book').get()
         .then((snapshot) => {
@@ -94,13 +127,96 @@ var Up_File = (req, res, next) => {
         } else {
             console.log("Upload is okay");
             console.log(req.file); // Thông tin file đã upload
-            if (req.file.filename) {
+            if (typeof req.file !== undefined && req.file) {
                 res.json({ "file": req.file.filename });
             } else {
-                res.json({ "result": 0 });
+                res.json({ "file": 0 });
             }
         }
 
     });
 }
-module.exports = { Create_Book, Up_File, Get_All, Delete_Book, Update_Book }
+
+var Update_File = (req, res, next) => {
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            console.log("A Multer error occurred when uploading.");
+        } else if (err) {
+            console.log("An unknown error occurred when uploading." + err);
+        } else {
+            console.log("Upload is okay");
+            console.log(req.file);
+
+            if (typeof req.file !== undefined && req.file) {
+                res.json({ "file": req.file.filename })
+            } else {
+                res.json({ "error": "404" })
+            }
+        }
+    });
+}
+
+
+var Get_All_Cate = (req, res) => {
+    let list = []
+    db.collection('Cate').get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                list.push(doc.data())
+            });
+            res.send(list)
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err);
+        });
+}
+var Delete_Cate = (req, res) => {
+    console.log(req.params.id)
+    db.collection("Cate").doc(req.params.id).delete()
+    res.send({ status: 200 })
+}
+var Update_Cate = (req, res) => {
+    console.log("body: " + req.body)
+
+    db.collection("Cate").doc(req.body.Cat_ID).update({
+        Name_Cat: req.body.Name_Cat,
+        Description_Cat: req.body.Description_Cat,
+        Publication_Date: req.body.Publication_Date,
+        Status_Cat: req.body.Status_Cat,
+        Time_Updated: '',
+    });
+
+    res.send({ status: 200 })
+}
+var Create_Cate = (req, res) => {
+
+    console.log(req.body)
+    console.log(Date.now())
+    let ref = db.collection('Cate').doc();
+    let id = ref.id;
+    db.collection('Cate').doc(id).set({
+        Cat_ID: id,
+        Name_Cat: req.body.Name_Cat,
+        Description_Cat: req.body.Description_Cat,
+        Publication_Date: req.body.Publication_Date,
+        Status_Cat: req.body.Status_Cat,
+        Time_Created: Date.now(),
+        Time_Updated: ''
+    });
+    res.send({ status: 200 })
+
+}
+
+module.exports = {
+    Create_Book,
+    Up_File,
+    Get_All_Book,
+    Delete_Book,
+    Update_Book,
+    Update_File,
+    Get_All_Cate,
+    Create_Cate,
+    Update_Cate,
+    Delete_Cate,
+    Get_All_User
+}
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,14 +1,36 @@
 const express = require("express");
-const { Create_Book, Up_File, Get_All, Delete_Book, Update_Book } = require("../controllers/Admin.Controller");
+const {
+    Create_Book,
+    Up_File,
+    Get_All_Book,
+    Delete_Book,
+    Update_Book,
+    Get_All_Cate,
+    Update_Cate,
+    Create_Cate,
+    Update_File,
+    Delete_Cate,
+    Get_All_User
+} = require("../controllers/Admin.Controller");
 const router = express.Router();
 let routerInit = (app) => {
     router.get("/", Create_Book);
     router.post("/create-book", Create_Book)
     router.delete("/delete-book/:id", Delete_Book)
     router.put("/update-book", Update_Book)
-
+    router.post("/update-file", Update_File)
     router.post("/file", Up_File)
-    router.get("/book", Get_All)
+    router.get("/book", Get_All_Book)
+
+    router.get("/cate", Get_All_Cate)
+    router.put("/update-cate", Update_Cate)
+    router.post("/create-cate", Create_Cate)
+    router.delete("/delete-cate/:id", Delete_Cate)
+
+    router.get("/user", Get_All_User)
+
+
+
     app.use("/api", router)
 }
 module.exports = routerInit;  
